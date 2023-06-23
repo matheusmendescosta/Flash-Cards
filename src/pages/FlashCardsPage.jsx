05e2870f932c90dcdd69c8bd8e5454289977196d
Matheus Mendes <matheus_mendess@live.com>
2023-06-22 23:54:36 -0300

    add react tabs

--- src/pages/FlashCardsPage.jsx
+++ src/pages/FlashCardsPage.jsx
@@ -1,4 +1,7 @@
 import React, { useState } from "react";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+
 import FlashCard from "../components/FlashCard/FlashCard";
 import { allFlashCards } from "../data/allFlashCards";
 import FlashCards from "../components/FlashCards/FlashCards";
@@ -26,33 +29,51 @@ function FlashCardsPage() {
   }
 
   return (
-    <Main>
-      <div>
-        <Header Description="Flash Cards" />
-      </div>
-      <div className="text-center mb-4">
-        <Button onButtonClick={handleButtonClick}>Embaralhar Cards</Button>
-      </div>
-      <div className="flex flex-row items-center justify-center space-x-2 m-4">
-        <RadioButton
-          id="radioButtonTitle"
-          name="showInfo"
-          buttonChecked={showTitle}
-          onButtonClick={handleRadioShowTitle}
-        />
-        <RadioButton
-          id="radioButtonDescription"
-          name="showInfo"
-          buttonChecked={!showTitle}
-          onButtonClick={handleRadioShowDescription}
-        />
-      </div>
-      <FlashCards>
-        {allCards.map(({ id, title, description }) => {
-          return <FlashCard key={id} title={title} description={description} showTitleFlashCard={showTitle} />;
-        })}
-      </FlashCards>
-    </Main>
+    <>
+      <Tabs>
+        <TabList>
+          <Tab>Listagem</Tab>
+          <Tab>Cadastro</Tab>
+          <Tab>Estudos</Tab>
+        </TabList>
+
+        <TabPanel>
+          <h2>Listagem</h2>
+        </TabPanel>
+        <TabPanel>
+          <h2>Cadastro</h2>
+        </TabPanel>
+        <Tab>
+          <Main>
+            <div>
+              <Header Description="Flash Cards" />
+            </div>
+            <div className="text-center mb-4">
+              <Button onButtonClick={handleButtonClick}>Embaralhar Cards</Button>
+            </div>
+            <div className="flex flex-row items-center justify-center space-x-2 m-4">
+              <RadioButton
+                id="radioButtonTitle"
+                name="showInfo"
+                buttonChecked={showTitle}
+                onButtonClick={handleRadioShowTitle}
+              />
+              <RadioButton
+                id="radioButtonDescription"
+                name="showInfo"
+                buttonChecked={!showTitle}
+                onButtonClick={handleRadioShowDescription}
+              />
+            </div>
+            <FlashCards>
+              {allCards.map(({ id, title, description }) => {
+                return <FlashCard key={id} title={title} description={description} showTitleFlashCard={showTitle} />;
+              })}
+            </FlashCards>
+          </Main>
+        </Tab>
+      </Tabs>
+    </>
   );
 }
 
